7630725ad138a0eeb1d30a6d07e12dfc150724c7
nielsen <nielsenrio@gmail.com>
2026-01-28 20:54:10 -0300

    feat: adicionado funcionalidade de layout Base e navegação com Django templates

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('', views.home, name='home'),
     path('contato/', views.contato, name='contato'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('ajuda/', views.ajuda, name='ajuda'),
     path('ola/<str:nome>/', views.saudacao, name='saudacao'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -11,7 +11,7 @@ def home(request):
         saudacao = "Boa tarde"
 
     contexto = {
-        "mensagem": f"{saudacao}! São {datetime.now().strftime("%H:%M")}h. Servidor ligado e respondendo. Atividade(s) da UC3, você colocou um back-end no ar.",
+        "mensagem": f"{saudacao}! São {datetime.now().strftime("%H:%M")}h. Servidor ligado e respondendo. Parabéns, você colocou um back-end no ar.",
         "nome_aluno": "Nielsen Moreira",
         "curso": "Programação em Python - Módulo UC3: Desenvolver aplicações back-end para web com Python"
     }
@@ -19,11 +19,17 @@ def home(request):
     return render(request, "pages/home.html", contexto)
 
 def contato(request):
-    nome = "Nielsen"
+    nome = "NCM"
     return render(request, "pages/contato.html", context={"nome":nome})
 
-def saudacao(request, nome):
+def saudacao(nome):
     return HttpResponse(
             f"<h1>Olá, {nome}! Bem-vindo(a) ao Senac!</h1>"
     )
+
+def sobre(request):
+    return render(request, 'pages/sobre.html')
+
+def ajuda(request):
+    return render(request, 'pages/ajuda.html')
 
